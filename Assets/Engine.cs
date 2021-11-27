71aeeab05b12cdf2e7371d63a41b38f23b161715
Aaron Hervey <>
2021-11-27 07:14:32 -0500

    Battle Engine Phase 1

--- Assets/Engine.cs
+++ Assets/Engine.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 public enum AllStates { START, PLAYER, ENEMY, TRANSITION, RESET }
 
 public class Engine : MonoBehaviour
-{ 
+{
     public Text currhp;
     public Text lvl;
     public Text lck;
     //public Text maxhp;
-    private Animation anim;
+    //private Animation anim;
     public GameObject enemyPrefab1, enemyPrefab2, enemyPrefab3;
     public GameObject playerPrefab;
+    public GameObject playerGO, enemy1GO, enemy2GO, enemy3GO;
+    public GameObject endTurnButton, attackButton;
+
+    private List<int> Attacks;
+    private List<UnitAttributes> Targets;
 
     public Transform playerSpawn;
     public Transform enemySpawn1, enemySpawn2, enemySpawn3;
@@ -24,15 +29,17 @@ public class Engine : MonoBehaviour
     UnitAttributes playerUnit;
     UnitAttributes enemyUnit1, enemyUnit2, enemyUnit3;
 
+    //private GameObject
+
     void Start()
     {
         state = AllStates.START;
-        StartBattle();
+        StartCoroutine(StartBattle());
     }
 
-    void StartBattle()
+    IEnumerator StartBattle()
     {
-        GameObject playerGO = Instantiate(playerPrefab, playerSpawn);
+        playerGO = Instantiate(playerPrefab, playerSpawn);
         playerUnit = playerGO.GetComponent<UnitAttributes>();
         currhp.text = playerUnit.unitCurrentHealth.ToString() + "/" + playerUnit.unitMaxHealth.ToString();
         lck.text = playerUnit.unitLuck.ToString();
@@ -41,20 +48,69 @@ public class Engine : MonoBehaviour
 
         if (enemyTrue1)
         {
-            GameObject enemy1GO = Instantiate(enemyPrefab1, enemySpawn1);
+            enemy1GO = Instantiate(enemyPrefab1, enemySpawn1);
             enemyUnit1 = enemy1GO.GetComponent<UnitAttributes>();
         }
 
         if (enemyTrue2)
         {
-            GameObject enemy2GO = Instantiate(enemyPrefab2, enemySpawn2);
+            enemy2GO = Instantiate(enemyPrefab2, enemySpawn2);
             enemyUnit2 = enemy2GO.GetComponent<UnitAttributes>();
         }
 
         if (enemyTrue3)
         {
-            GameObject enemy3GO = Instantiate(enemyPrefab3, enemySpawn3);
+            enemy3GO = Instantiate(enemyPrefab3, enemySpawn3);
             enemyUnit3 = enemy3GO.GetComponent<UnitAttributes>();
         }
+        state = AllStates.PLAYER;
+        yield return new WaitForSeconds(2f);
+        PlayerTurn();
     }
+
+    void PlayerTurn()
+    {
+        Attacks = new List<int>();
+        Targets = new List<UnitAttributes>();
+
+    }
+    public void checkEndTurn()
+    {
+        if (state != AllStates.PLAYER)
+        {
+            return;
+        }
+        state = AllStates.TRANSITION;
+        Transition();
+    }
+
+    public void attackCheck()
+    {
+        if (state != AllStates.PLAYER)
+        {
+            return;
+        }
+        Attacks.Add(playerUnit.attack);
+        Targets.Add(enemyUnit1);
+    }
+
+    IEnumerator PlayerAttack(UnitAttributes enemyunit, int attack)
+    {
+
+        enemyunit.takeDamage(attack);
+        yield return new WaitForSeconds(2f);
+   
+
+    }
+
+    void Transition()
+    {
+        for (int i = 0; i < Attacks.Count; i++)
+        {
+            StartCoroutine(PlayerAttack(Targets[i], Attacks[i]));
+
+        }
+        //yield return new WaitForSeconds(2f);
+    }
+    
 }
--- Assets/UnitAttributes.cs
+++ Assets/UnitAttributes.cs
@@ -10,5 +10,16 @@ public class UnitAttributes : MonoBehaviour
     public int unitCurrentHealth;
     public int unitLuck;
     public int unitLevel;
+    public int numAttacks;
+    public int attack;
+    public int defense;
+    public void takeDamage(int damage)
+    {
+        Animator anim;
+        anim = GetComponentInChildren<Animator>();
+        anim.Play("Hurt");
+
+        unitCurrentHealth = unitCurrentHealth - damage;
+    }
 
 }
