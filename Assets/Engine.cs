b537ebba16caf6d2faf9346581b68b1710eaedb2
Aaron Hervey <>
2021-11-27 21:27:21 -0500

    Battle Engine Phase 2

--- Assets/Engine.cs
+++ Assets/Engine.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public enum AllStates { START, PLAYER, ENEMY, TRANSITION, RESET }
+public enum AllStates { START, PLAYER, ENEMY, TRANSITION, RESET, PROGRESS }
 
 public class Engine : MonoBehaviour
 {
     public Text currhp;
     public Text lvl;
     public Text lck;
+    public int backgroundSelector;
     //public Text maxhp;
     //private Animation anim;
     public GameObject enemyPrefab1, enemyPrefab2, enemyPrefab3;
+    public GameObject background, backgroundPrefab;
     public GameObject playerPrefab;
     public GameObject playerGO, enemy1GO, enemy2GO, enemy3GO;
     public GameObject endTurnButton, attackButton;
 
+    public Map map;
     private List<int> Attacks;
     private List<UnitAttributes> Targets;
 
     public Transform playerSpawn;
+    public Transform backgroundAnchor;
     public Transform enemySpawn1, enemySpawn2, enemySpawn3;
     public bool enemyTrue1, enemyTrue2, enemyTrue3;
 
@@ -33,12 +38,15 @@ public class Engine : MonoBehaviour
 
     void Start()
     {
+
         state = AllStates.START;
         StartCoroutine(StartBattle());
     }
 
     IEnumerator StartBattle()
     {
+        background = Instantiate(backgroundPrefab, backgroundAnchor); 
+
         playerGO = Instantiate(playerPrefab, playerSpawn);
         playerUnit = playerGO.GetComponent<UnitAttributes>();
         currhp.text = playerUnit.unitCurrentHealth.ToString() + "/" + playerUnit.unitMaxHealth.ToString();
@@ -63,6 +71,7 @@ public class Engine : MonoBehaviour
             enemy3GO = Instantiate(enemyPrefab3, enemySpawn3);
             enemyUnit3 = enemy3GO.GetComponent<UnitAttributes>();
         }
+
         state = AllStates.PLAYER;
         yield return new WaitForSeconds(2f);
         PlayerTurn();
@@ -111,6 +120,58 @@ public class Engine : MonoBehaviour
 
         }
         //yield return new WaitForSeconds(2f);
+        state = AllStates.ENEMY;
+        enemyTurn();
     }
-    
+
+    void enemyTurn()
+    {
+        if (enemyTrue1)
+        {
+            StartCoroutine(enemyAttack(enemyUnit1));
+        }
+
+        if (enemyTrue2)
+        {
+            StartCoroutine(enemyAttack(enemyUnit2));
+        }
+
+        if (enemyTrue3)
+        {
+            StartCoroutine(enemyAttack(enemyUnit3));
+        }
+        if (enemyTrue1 || enemyTrue2 || enemyTrue3)
+        {
+            state = AllStates.PLAYER;
+            PlayerTurn();
+        }
+        else
+        {
+            state = AllStates.PROGRESS;
+            progress();
+
+        }
+        
+    }
+
+    IEnumerator enemyAttack(UnitAttributes enemy)
+    {
+        playerUnit.takeDamage(enemy.attack);
+        if (playerUnit.unitCurrentHealth == 0)
+        {
+            state = AllStates.RESET;
+            endGame();
+        }
+        yield return new WaitForSeconds(2f);
+    }
+
+    void endGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void progress()
+    {
+        SceneManager.LoadScene(2);
+        StartBattle();
+    }
 }
--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Map map;
     // Start is called before the first frame update
     public void StartGame(){
+        map.generateMap();
         SceneManager.LoadScene(1);
     }
     
